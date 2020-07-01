62cd910494a39fbc70861813446f9cebb777849d
Regan Meloche <reganmeloche@gmail.com>
2020-06-30 19:19:24 -0600

    added bloodbank folder

--- Program.cs
+++ Program.cs
@@ -6,31 +6,7 @@ namespace Unit3
     {
         static void Main(string[] args)
         {
-            // Define all the blood types
-            var bloodTypeOn = new BloodType("O", '-');
-            var bloodTypeOp = new BloodType("O", '+');
-            var bloodTypeAn = new BloodType("A", '-');
-            var bloodTypeAp = new BloodType("A", '+');
-            var bloodTypeBn = new BloodType("B", '-');
-            var bloodTypeBp = new BloodType("B", '+');
-            var bloodTypeABn = new BloodType("AB", '-');
-            var bloodTypeABp = new BloodType("AB", '+');
 
-            // Create a donor
-            var dob1 = new DateTime(1960, 2, 3);
-            var donor1 = new Donor("Homer", "Simpson", dob1, bloodTypeBn);
-            donor1.PrintDetails();
-
-            // Create another donor
-            var dob2 = new DateTime(1994, 11, 21);
-            var donor2 = new Donor("Bart", "Simpson", dob2, bloodTypeBn);
-            donor2.PrintDetails();
-
-            // Create a receiver
-            var receiver = new Receiver("Marge", "Simpson", new DateTime(1960, 4, 18), bloodTypeAp);
-
-            donor1.TryToMakeDonation();
-            donor1.TryToMakeDonation();
         }
     }
    
